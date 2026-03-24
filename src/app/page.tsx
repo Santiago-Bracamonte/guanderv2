899f22426fa257c4e516d94fa556eeb9748309b2
Tgonzdv <tomas.gonzalezz@davinci.edu.ar>
2026-03-24 16:20:05 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import HeroBanner from "./components/HeroBanner";
 import StatsBar from "./components/StatsBar";
 import LocationsFilterSection from "./components/LocationsFilterSection";
 import ExclusiveOffersSection from "./components/ExclusiveOffersSection";
-import StoresList from "./components/StoresList";
 import SubscriptionPlans from "./components/SubscriptionPlans";
 import DownloadsSection from "./components/DownloadsSection";
 import Footer from "./components/Footer";
